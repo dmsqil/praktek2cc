cffda4b4f6160e16280c3a0a6d27d46e38bb5412
Dimasaqil <dimasaqilsalsabil@gmail.com>
2025-10-10 14:08:52 +0700

    Update Index.php

--- var/www/myapp/index.php
+++ var/www/myapp/index.php
@@ -1,4 +1,5 @@
 <?php
-echo "<h1>Halo dari Oracle Cloud!</h1>";
+echo "<h1>Halo dari Oracle Cloud!</h1>"
+echo "<h2>Halo dari Cloud Computing</h2>";
 echo "<p>Deployed dengan Nginx dan CI/CD GitHub Actions</p>";
 ?>
